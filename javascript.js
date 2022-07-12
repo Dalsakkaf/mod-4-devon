ae59699abb73af8a263f0c460e384a39b9dd7a6f
Dalsakkaf <105419100+Dalsakkaf@users.noreply.github.com>
2022-07-12 12:59:55 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+var startBtn = document.querySelector("#start");
+
+var questions = [{
+    question: "1. What does HTML stand for?",
+    choices: ["Hyper Text Markup Language", "How To Milk Lizards", "Harold Tony Marvin Larry"],
+    correctAnswer: 0
+}, {
+    question: "1. Which one is responsible for page styling?",
+    choices: ["CSS", "Javascript", "HTML"],
+    correctAnswer: 0
+}, {
+    question: "1. What does HTML stand for?",
+    choices: ["Hyper Text Markup Language", "How To Milk Lizards", "Harold Tony Marvin Larry"],
+    correctAnswer: 0
+}];
+
+function onclick
+
+startBtn.addEventListener("click");
